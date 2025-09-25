c95c51c07bbb092a7c8706c6db3f3c379b681d55
Dazai_Osamu <zerospeciman50@gmail.com>
2025-09-25 18:39:59 +0530

    Login Page 1 Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from "../app/components/home/home";
+import { Admin } from './components/admin/admin';
+import { NotFound } from './components/not-found/not-found';
+import { Login1 } from './components/login1/login1';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: Home
+  },
+  {
+    path: '',
+    component: Login1
+  },
+  {
+    path: 'admin',
+    component: Admin
+  },
+  {
+    path: '**',
+    component: NotFound
+  },
+];
