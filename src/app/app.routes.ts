20775c3052d190b1f0aedc520b23b4ef78f62faf
Dazai_Osamu <zerospeciman50@gmail.com>
2026-01-09 11:08:51 +0530

    home component removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./components/home/home').then(m => m.Home)
-  },
-  {
-    path: 'sliderLogin',
+    path: '',
     loadComponent: () => import('./components/slider-login/slider-login').then(m => m.SliderLogin)
   },
   {
     path: 'login1',
     loadComponent: () => import('./components/login1/login1').then(m => m.Login1)
-  },
-  {
-    path: 'admin',
-    loadComponent: () => import('./components/admin/admin').then(m => m.Admin)
   },
   {
     path: '**',
